d1894bef9c116c55572014ed9d98040d9d6441d9
Andrew Koster <andrewkoster95@gmail.com>
2021-08-24 23:44:07 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { typeDefs, resolvers } = require("./schemas");
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const server = new ApolloServer({
@@ -24,6 +24,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
