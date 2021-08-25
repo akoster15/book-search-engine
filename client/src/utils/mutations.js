5ae4bbbd11b43ae689a89c34560554da370ba9d0
Andrew Koster <andrewkoster95@gmail.com>
2021-08-24 20:03:35 -0400

    updated mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,19 +1,64 @@
 import { gql } from '@apollo/client'
 
-export const GET_ME = gql`
-{
-    me {
-        _id
-        username
-        email
-        bookCount
-        savedBooks{
-            bookId
-            authors
-            description
-            title
-            image
-            link
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
         }
     }
-}`;
+`;
+
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+        }
+        
+    }
+    `;
+
+    export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+        }
+        
+    }
+    `;
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }    
+    }
+`; 
